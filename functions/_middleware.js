d6b7c8f13877e1a9907de232febac3d5a0b15ac4
monkclock65 <zackinceok17@gmail.com>
2025-10-03 19:18:46 -0500

    test

--- functions/_middleware.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest({ request, env }) {
-  // Forward the request to your worker
-  const workerUrl = "https://cf-ai-book-recommender.workers.dev";
-  return fetch(new Request(workerUrl, request));
-}
